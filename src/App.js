554dc5b682f78907f36583a8918b5951c9f22ea5
inhye kim <ggg6519@gmail.com>
2022-08-10 08:26:04 +1200

    reset gh-page

--- src/App.js
+++ src/App.js
@@ -10,10 +10,10 @@ function App() {
   return (
     <>
       <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/About" element={<About />} />
-        <Route path="/Project" element={<Project />} />
-        <Route path="/Contact" element={<Contact />} />
+        <Route path="/Amy_Portfolio/" element={<Home />} />
+        <Route path="/Amy_Portfolio/About" element={<About />} />
+        <Route path="/Amy_Portfolio/Project" element={<Project />} />
+        <Route path="/Amy_Portfolio/Contact" element={<Contact />} />
       </Routes>
     </>
   )
--- src/index.js
+++ src/index.js
@@ -5,7 +5,7 @@ import App from './App'
 import { BrowserRouter } from 'react-router-dom'
 
 ReactDOM.render(
-  <BrowserRouter beasename={window.location.pathname || ''}>
+  <BrowserRouter>
     <App />
   </BrowserRouter>,
   document.getElementById('root')
